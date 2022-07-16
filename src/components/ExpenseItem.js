8f5a88464e11b9fa83e6af0160434dfd6ebb1858
chinthaka <chinthakarukshan@gmail.com>
2022-07-17 00:20:15 +0530

    Added few complex items to the new component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$543.45</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
