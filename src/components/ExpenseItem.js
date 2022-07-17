94ac54a9b8ae3c02e91e69be75a89e8e6d29da3f
chinthaka <chinthakarukshan@gmail.com>
2022-07-18 00:31:29 +0530

    Extend the component to inject values using variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2021,2,25);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 543.45;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$543.45</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
