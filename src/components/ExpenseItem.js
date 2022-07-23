21b6c5dcdb56310ba6d0f296f66dd4f855a22a96
chinthaka <chinthakarukshan@gmail.com>
2022-07-23 23:58:07 +0530

    Using props for passing values to build reusable components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
     const expenseDate = new Date(2021,2,25);
     const expenseTitle = 'Car Insurance';
@@ -8,10 +8,10 @@ function ExpenseItem() {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
