6e253a0682deeed8ffd7df0add68a0a29296273e
chinthaka <chinthakarukshan@gmail.com>
2022-08-12 00:01:42 +0530

    Added new expense form and new expense component for the users to submit new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
@@ -28,7 +29,7 @@ const expenses = [
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses}/>
     </div>
   );
