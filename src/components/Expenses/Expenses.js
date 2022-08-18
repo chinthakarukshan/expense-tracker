47a2472160a1b0270a1ead3d4dc22a1b8359424b
chinthaka <chinthakarukshan@gmail.com>
2022-08-18 23:02:49 +0530

    Added key to the list to solve issues when rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,7 +18,7 @@ const Expenses = (props) => {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selectedYear={filteredYear} onChangeDropdown={changeDropDownValue}/>
-                {expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+                {expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
             </Card>
         </div>
     );
